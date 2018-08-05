31d9bb84c8e79a37655f50ef471b9136125fa96d
CarloCarpio <carlo.carpio@narrasoft.com>
2018-08-05 22:41:20 +0800

    Added Profile Image.

--- InstagramFirebase/ViewController.swift
+++ InstagramFirebase/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 //import Firebase
 import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
         return button
     }()
     
@@ -67,6 +68,28 @@ class ViewController: UIViewController {
         return button
     }()
     
+    @objc func handlePlusPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width/2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+        plusPhotoButton.layer.borderWidth = 3
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
     @objc func handleTextInputChange() {
         let isFormValid = emailTextField.text?.characters.count ?? 0 > 0 && usernameTextField.text?.characters.count ?? 0 > 0 && passwordTextField.text?.characters.count ?? 0 > 0
         
@@ -93,17 +116,42 @@ class ViewController: UIViewController {
             
             print("Succesfull adding user", user?.user.uid ?? "")
             
-            guard let uid = user?.user.uid else { return }
+            guard let image = self.plusPhotoButton.imageView?.image else { return }
+            guard let uploadData = UIImageJPEGRepresentation(image, 0.3) else { return }
+            let filename = NSUUID().uuidString
             
-            let values = [uid: 1]
-            Database.database().reference().child("users").setValue(values, withCompletionBlock: { (err, ref) in
+            let storageRef = Storage.storage().reference()
+            let storageRefChild = storageRef.child("profile_images").child(filename)
+            
+            storageRefChild.putData(uploadData, metadata: nil, completion: { (metadata, err) in
                 if let err = err {
-                    print("Failed to save user info into db: ", err)
-                    return
+                    print("Unable to upload image into storage due to: \(err)")
                 }
                 
-                print("Successfully save user to db.")
+                storageRefChild.downloadURL(completion: { (url, err) in
+                    if let err = err {
+                        print("Unable to retrieve URL due to error: \(err.localizedDescription)")
+                        return
+                    }
+                    let profilePicUrl =  url?.absoluteString
+                    print("Profile Image successfully uploaded into storage with url: \(profilePicUrl ?? "" )")
+                    
+                    guard let uid = user?.user.uid else { return }
+                    
+                    let usernameValues = ["username": username, "profileImageUrl": profilePicUrl]
+                    let values = [uid: usernameValues]
+                    
+                    Database.database().reference().child("users").updateChildValues(values, withCompletionBlock: { (err, ref) in
+                        if let err = err {
+                            print("Failed to save user info into db: ", err)
+                            return
+                        }
+                        
+                        print("Successfully save user to db.")
+                    })
+                })
             })
+            
         }
     }
     
